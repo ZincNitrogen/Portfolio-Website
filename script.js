6a20ff35bb798249e2f4dfada682aa0c9d468d3e
Ziyad Noorhassan <67880504+thEchroniCamateuR@users.noreply.github.com>
2024-05-13 17:59:30 -0400

    Minor html edits. First javascript on this site
    Learned that <script> tags should be moved to the <body> tag because of synchronous reasons.

---
+++ script.js
@@ -0,0 +1,16 @@
+let hornyjail = document.querySelector("#developer");
+
+hornyjail.onmouseover = function() {
+
+    this.innerHTML = "Hire Me";
+    return hornyjail;
+
+    
+};
+
+hornyjail.onmouseout = function () {
+
+    this.innerHTML = "Developer";
+    return hornyjail;
+};
+
