216ffca74e14d1a496702f1b5c39c323a3e2d1c5
Ziyad Noorhassan <67880504+thEchroniCamateuR@users.noreply.github.com>
2024-05-17 22:01:14 -0400

    javascript on Knowledgebase page, added script tag to bottom of html files.

--- script.js
+++ script.js
@@ -1,16 +1,93 @@
-let hornyjail = document.querySelector("#developer");
 
-hornyjail.onmouseover = function() {
+let bruh = document.querySelector("#developer");
 
-    this.innerHTML = "Hire Me";
-    return hornyjail;
 
-    
-};
+function devFunc() {
 
-hornyjail.onmouseout = function () {
+    bruh.textContent = "Hire me";
+}
 
-    this.innerHTML = "Developer";
-    return hornyjail;
-};
+
+function bruhFuncTwo(){
+
+    bruh.textContent = "Developer";
+}
+
+
+
+
+
+let hireAll = document.querySelectorAll(".hire");
+let meAll = document.querySelectorAll(".me");
+
+let hire = document.querySelector(".hire");
+let me = document.querySelector(".me");
+
+let hireArray = Array.from(hireAll);
+let meArray = Array.from(meAll);
+
+
+
+function hireFuncColor(){
+
+
+    this.style.color = "blue";
+    this.style.opacity = "100";
+
+}
+
+function hireFuncNonColor (){
+
+    this.style.color = "var(--grey_light)";
+    this.style.opacity = "5%";
+
+}
+
+function meFuncColor(){
+
+    this.style.color = "orange";
+    this.style.opacity = "100";
+
+}
+
+function meFuncNonColor (){
+
+    this.style.color = "grey";
+    this.style.opacity = "5%";
+
+}
+
+
+
+
+
+
+
+
+for (i of hireArray) {
+    i.addEventListener("mouseover", hireFuncColor);
+    i.addEventListener("mouseout", hireFuncNonColor);
+}
+
+for (i of meArray) {
+    i.addEventListener("mouseover", meFuncColor);
+    i.addEventListener("mouseout", meFuncNonColor);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+bruh.addEventListener("mouseover", devFunc);
+bruh.addEventListener("mouseout", bruhFuncTwo);
+
+
 
